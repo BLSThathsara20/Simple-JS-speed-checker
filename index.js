1617e341caa1084ba0a8ad51b3328a9ded36a344
Savindu <blsthathsara@gmail.com>
2021-09-29 23:55:51 +0530

    Problem identify Signed-off-by: Savindu <blsthathsara@gmail.com>

---
+++ index.js
@@ -0,0 +1,15 @@
+// Speed Limit = 70
+// 5 -> 1 point
+// Math.floor(1.3)
+//12 points -> License suspended
+
+let speed = checkSpeed(55);
+console.log(speed);
+
+function checkSpeed(speed){
+    if(speed <= 70){
+        return 'OK'
+    }
+
+    for()
+}
